425da4bc6d88d536d10cb48cb44fea5ef2872591
priyanka.karuppusamy <priyanka.karuppusamy@wipro.com>
2026-07-15 11:39:13 +0530

    fixed vulnerabilities

--- app.js
+++ app.js
@@ -1,46 +1,68 @@
-
 const express = require("express");
 const child_process = require("child_process");
 const fs = require("fs");
 const path = require("path");
 const app = express();
 const port = 3000;
+function validateHost(host){
+   return /^[A-Za-z0-9.-]+$/
+           .test(host);
+}
+function escapeHtml(text){
+ return text
+  .replaceAll("<","&lt;")
+  .replaceAll(">","&gt;");
+}
 /*
-Vulnerability 1
-Command Injection
+FIX COMMAND INJECTION
 */
-app.get("/ping", (req, res) => {
-    const host = req.query.host;
-    child_process.exec(
-        "ping -c 1 " + host,
-        (err, stdout) => {
-            res.send(stdout);
-        }
-    );
+app.get("/ping", (req,res)=>{
+ const host =
+      req.query.host;
+ if(!validateHost(host))
+ {
+   return res.status(400)
+             .send("Invalid");
+ }
+ child_process.execFile(
+    "ping",
+    ["-c","1",host],
+    (err,stdout)=>{
+      res.send(stdout);
+    }
+ );
 });
 /*
-Vulnerability 2
-Path Traversal
+FIX PATH TRAVERSAL
 */
-app.get("/read", (req, res) => {
-    const fileName = req.query.file;
-    const filePath = path.join(
-        __dirname,
-        fileName
-    );
-    const content =
-        fs.readFileSync(filePath);
-    res.send(content);
+app.get("/read",(req,res)=>{
+ const allowed =
+    ["README.md"];
+ const file =
+    req.query.file;
+ if(
+   !allowed.includes(file)
+ ){
+   return res.send(
+      "Not Allowed"
+   );
+ }
+ const content=
+   fs.readFileSync(file);
+ res.send(content);
 });
 /*
-Vulnerability 3
-Reflected XSS
+FIX XSS
 */
-app.get("/hello", (req, res) => {
-    const name = req.query.name;
-    res.send(
-        "<h1>Hello " + name + "</h1>"
-    );
+app.get("/hello",(req,res)=>{
+ const safeName=
+ escapeHtml(
+    req.query.name
+ );
+ res.send(
+  "<h1>Hello "
+  + safeName
+  +"</h1>"
+ );
 });
 app.listen(port);
-
